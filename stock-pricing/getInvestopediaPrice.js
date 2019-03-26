7a3c02ae64d2356cb4640e94ff2b2ace89287af4
Jack Hedaya <jackehedaya@gmail.com>
2019-03-26 00:45:29 -0400

    investopedia price is pulled successfully

--- stock-pricing/getInvestopediaPrice.js
+++ stock-pricing/getInvestopediaPrice.js
@@ -1,34 +1,32 @@
 const puppeteer = require("puppeteer");
 
 /**
- * Gets stock prices exactly 5 minutes earlier
- * @param {string[] | string} ticker an individual ticker or multiple tickers seperated by spaces
+ * Gets investopedia stock price
+ * @param {string} ticker a stock ticker
+ * @returns {Promise<Number>} the stock price
  */
-async function getInvestopediaPrice(stock) {
-  if (!global.browser)
-  global.browser = await puppeteer.launch({
-      headless: false,
-      userDataDir: "/Users/jachedaya/Library/Application Support/Google/Chrome/Default"
-    });
+async function getInvestopediaPrice(ticker) {
+  let browser = global.browser;
 
   const page = await browser.newPage();
 
   const navigationPromise = page.waitForNavigation();
 
   await page.goto("https://www.investopedia.com/simulator/trade/tradestock.aspx");
-  await page.type("input#symbolTextbox", stock);
+  await page.type("input#symbolTextbox", ticker);
   await page.click(".simulator-page");
 
   try {
     await page.waitForFunction(_ => document.querySelectorAll("td.num").length > 4);
-    const price = await page.evaluate(_ => document.querySelectorAll("td.num")[4].textContent);
+    const price = (await page.evaluate(_ => document.querySelectorAll("td.num")[4].textContent)).replace(/,/g, "");
 
     await navigationPromise;
 
     await page.close();
     return parseFloat(price);
   } catch (x) {
-    console.log(`Failed getting ${ticker} price`);
+    console.log(`Failed getting investopedia ${ticker} price`);
+
     return;
   }
 }

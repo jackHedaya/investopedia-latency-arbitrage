28a903a81634a926b7e856134384d95979e06532
Jack Hedaya <jackehedaya@gmail.com>
2019-03-26 01:39:44 -0400

    moved stock list to configuration

---
+++ config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  /**
+   * the tickers that will be traded back and forth
+   */
+  tickers: [
+    "CPRX",
+    "AXSM",
+    "YRD",
+    "NIKE",
+    "ROKU",
+    "PCG",
+    "SRNE",
+    "KNDI",
+    "CCXI",
+    "TPNL",
+    "PVAC",
+    "LXRX",
+    "DSW",
+    "TSLA",
+    "AAPL",
+    "CWH"
+  ],
+
+  /**
+   * the amount of money that is allocated to trading
+   */
+  WORKING_WITH: 250000,
+
+  /**
+   * the amount of top movers from the ticker list that will be traded
+   */
+  AMOUNT_OF_STOCKS: 4
+}

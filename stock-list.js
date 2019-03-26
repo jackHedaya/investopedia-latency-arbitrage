ab3e04ddc08abea2530bd86ae939979387b2739b
Jack Hedaya <jackehedaya@gmail.com>
2019-03-26 01:12:21 -0400

    refactoring and reorganization

---
+++ stock-list.js
@@ -0,0 +1,18 @@
+module.exports = [
+  "CPRX",
+  "AXSM",
+  "YRD",
+  "NIKE",
+  "ROKU",
+  "PCG",
+  "SRNE",
+  "KNDI",
+  "CCXI",
+  "TPNL",
+  "PVAC",
+  "LXRX",
+  "DSW",
+  "TSLA",
+  "AAPL",
+  "CWH"
+];
